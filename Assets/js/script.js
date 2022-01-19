ebbfb5886b9eaeec0ebead641e082ed4fac7b16b
Jacob Lewis <jacobllewis@outlook.com>
2022-01-18 20:45:44 -0700

    Initial commit.

---
+++ Assets/js/script.js
@@ -0,0 +1,26 @@
+// Questions/possible answers x5?
+// correct answer
+
+// variables for question div
+// variables for submit button
+// variables for timer on screen
+// variable for start button
+
+// playGame
+    // Start timer - reduce from 30
+    // display question
+    
+    // if incorrect answer -10 seconds & next question
+    // else next question
+
+    // if time = 0 or no more questions = game over (run gameOver)
+
+// funtion gameOver
+// ask for initials - local storage
+// score = time remaining - local storage
+
+// order leaderboard by score - highest to lowest
+
+// display leaderboard from local storage
+
+// on start game ("click", playGame);
