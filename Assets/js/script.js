10ab3406c2c140c3bf78aaf19130f3054610ee55
Jacob Lewis <jacobllewis@outlook.com>
2022-01-19 19:25:27 -0700

    Created questions.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,22 @@
-// Questions/possible answers x5?
-// correct answer
+// Questions/possible answers x5? with correct answer
+var questions = [
+    {question: "Where does meta data go?",
+    a: "Body", b: "Link", c: "Head", d: "Footer",
+    answer: "Head"},
+
+    {question: "Where do Javascript links go?",
+    a: "Head", b: "Footer", c: "Body", d: "Div",
+    answer: "Body"},
+
+    {question: "What is the file extension for Javascript?",
+    a: ".pdf", b: ".txt", c: ".html", d: ".js",
+    answer: ".js"},
+
+    {question: "How many children can an element have?",
+    a: "1", b: "7", c: "As many as they want, bro!", d: "That's kinda personal...",
+    answer: "As many as they want, bro!"}
+]
+
 
 // variables for question div
 // variables for submit button
